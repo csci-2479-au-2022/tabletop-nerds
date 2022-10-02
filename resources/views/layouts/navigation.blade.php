23eed90a6aff8274b9bc891819500b9e56ae68eb
Michael McAdow <myup@DESKTOP-NHIIBBQ.localdomain>
2022-10-02 16:22:56 -0400

    Draft to be reviewed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,11 @@
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
-
+                <!--Search textbox -->
+                <form action="" method="GET">
+                    <input type="text" name="text" class="search" placeholder="What do you want?">
+                    <input type="submit" name="submit" class="submit" value="Search">
+                </form>
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -67,6 +71,11 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <!--Search textbox -->
+        <form action="" method="GET">
+            <input type="text" name="text" class="search" placeholder="What do you want?">
+            <input type="submit" name="submit" class="submit" value="Search">
+        </form>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
