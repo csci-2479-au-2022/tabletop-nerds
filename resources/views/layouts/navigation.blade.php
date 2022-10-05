c2e328ebff37a8ec9c9dfeeca3b3595ef4fb80e4
Jessica Epley <jess@Jess-Laptop>
2022-10-04 23:53:30 -0400

    Final final commit that I actually updated the navigation correctly to be part of the dropdown menu when logged in.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -48,30 +48,10 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Wishlist added to dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('wishlist')" :active="request()->routeIs('wish-list')">
+                        <!-- Wishlist added to dropdown -->
+                        <x-dropdown-link :href="route('wish-list')" :active="request()->routeIs('wish-list')">
                             {{ __('Your Wishlist') }}
                         </x-dropdown-link>
-                        </form>
                     </x-slot>
                 </x-dropdown>
             </div>
@@ -119,15 +99,11 @@
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
-            </div>
-
-            <!-- Adding Wishlist option to dropdown menu -->
-            <div class="mt-3 space-y-1">
+                <!-- Adding Wishlist option to dropdown menu -->
                 <x-responsive-nav-link :href="route('wish-list')" :active="request()->routeIs('wish-list')">
                     {{ __('Your Wishlist') }}
                 </x-responsive-nav-link>
             </div>
-
         </div>
     </div>
 </nav>
