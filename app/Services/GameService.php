da18d8983dbe8ad382f4ad9d3daa696169f1032c
Lil Dead01 <lildead01@icloud.com>
2022-10-15 09:52:53 -0400

    basic changes need commit before switching

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -4,14 +4,27 @@ namespace App\Services;
 
 use App\Models\Wishlist;
 
-function getGamesById(int $id) {
+function getGamesById(int $id): Wishlist {
+    $games = $this->getGames();
 
+    foreach ($games as $game) {
+        if ($game->id === $id) {
+            return $game;
+        }
+    }
 }
 
 function getGames ($orderBy, $direction, $limit) {
 
 }
 
-function searchGamesByTitle($title) {
+function searchGamesByTitle(string $title) {
+$games = $this->getGames();
 
+foreach ($games as $game) {
+    if (strtolower($game->title) === strtolower($title)) {
+        $foundTitle = $title;
+        break;
+    }
+}
 }
