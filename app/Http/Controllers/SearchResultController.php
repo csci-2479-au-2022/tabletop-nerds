719398955ff9c5153e66bc75f46789adc567c876
Lil Dead01 <lildead01@icloud.com>
2022-10-19 18:45:42 -0400

    idr what i did

--- app/Http/Controllers/SearchResultController.php
+++ app/Http/Controllers/SearchResultController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Services\GameService;
 
-class SearchResultController extends Controller
+class SearchResultController implements GameService
 {
     public function displayResults() {
-        return view('search-results');
+        return view('search-results', [
+            'title'=>$this->searchGamesByTitle()
+        ]);
     }
 }
--- resources/views/game-list.blade.php
+++ resources/views/game-list.blade.php
@@ -1,16 +1,15 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Game List</title>
 </head>
 <body>
     <h1>Game List</h1><h2>{{$greeting}}</h2>
     @foreach($games as $game)
     <p>{{$game['title']}}</p>
-    @endforeach 
+    @endforeach
 </body>
 </html>
