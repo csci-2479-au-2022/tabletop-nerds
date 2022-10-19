14fadfd1c5d7634fbab3dc881e13eab150dc3af6
Jessica Epley <jess@Jess-Laptop>
2022-10-18 21:59:52 -0400

    Added initial migrations that will create games, categories, and publishers tables.

---
+++ database/migrations/2022_10_19_013449_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->decimal('complexity_rating', 3, 2);
+            $table->year('release_year');
+            $table->foreignId('publisher_id');
+            $table->integer('playing_time_minutes')->unsigned();
+            $table->integer('min_number_players')->unsigned();
+            $table->integer('max_number_players')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
