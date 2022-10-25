aa6819d037d426acd6fed0535c9559054d15a239
Michelle D <miche@DESKTOP-KU18EC1.localdomain>
2022-10-25 15:54:16 -0400

    Changes made to accountControllerTest

--- tests/Feature/accountControllerTest.php
+++ tests/Feature/accountControllerTest.php
@@ -19,21 +19,26 @@ class accountControllerTest extends TestCase{
 
     public function test_get_wishlist_returns_ok()
     {
+        $expectedGames = [
+            ['title'=>'Catan'],
+            ['title'=>'Risk'],
+            ['title'=>'Ticket to Ride'],
+        ];
+
         $response = $this->get('/wishlist');
+        $response->assertViewHas('wishlist', $expectedGames);
+    }
+
+    public function test_get_wishlist_by_id_returns_ok()
+    {
+        $response = $this->get('/wishlist/{id}');
         $response->assertStatus(200);
     }
 
-    // public function test_get_wishlist_by_id_returns_ok()
-    // {
-    //     $response = $this->get('/wishlist/{id}');
-    //     $response->assertStatus(200);
-    // }
-
-    // public function test_get_wishlist_by_id_returns_404()
-    // {
-    //     $repsonse = $this->get('/wishlsit/4');
-    //     $repsonse->assertStatus(404);
-    // }
-
+    public function test_get_wishlist_by_id_returns_404()
+    {
+        $repsonse = $this->get('/wishlist/4');
+        $repsonse->assertStatus(404);
+    }
 
 }
