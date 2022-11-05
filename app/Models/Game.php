b633da8f8d61c084d6bb5c1db0839c5c03cd90bc
Jessica Epley <jess@Jess-Laptop>
2022-11-05 14:06:43 -0400

    Updated Game model fillable array to match games table in database

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
-    protected $fillable = ['id', 'title', 'rating', 'age', ];
+    protected $fillable = ['id', 'title', 'complexity_rating', 'release_year', 'publisher', 'playing_time_minutes', 'min_number_players', 'max_number_playeres'];
 
     public function toString(): string
     {
-        return "$this->title, Rating: $this->rating, Recommended age: $this->age";
+        return "$this->title, Complexity Rating: $this->complexity_rating, Publisher: $this->publisher, Average game time: $this->playing_time_minutes, Players: $this->min_number_players - $this->max_number_players";
     }
 
     public function publisher() {
