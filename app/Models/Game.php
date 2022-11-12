fa9605fb2dea31db03294e8b1b856ecb9101a6df
Michael McAdow <myup@DESKTOP-NHIIBBQ.localdomain>
2022-11-12 17:09:07 -0500

    last update 11/12 resolves #34

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'id',
         'title',
