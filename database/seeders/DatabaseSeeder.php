2e91609cb559cd19a0c2a18299cc776742bbcdde
Michael McAdow <myup@DESKTOP-NHIIBBQ.localdomain>
2022-11-13 11:14:02 -0500

    another update, 11/13 resolves #34 when approved

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        GameFactory::factory(10)->create();
-
         $this->call([
             PublisherSeeder::class,
             CategorySeeder::class,
             GameSeeder::class,
         ]);
-
     }
 
 }
