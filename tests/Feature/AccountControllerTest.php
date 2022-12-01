c2c15e0d184984a2600aaa9d155ffa83355116ab
Andrew Harris <aharri10@cscc.edu>
2022-11-30 21:18:46 -0500

    test: fix wishlist test

--- tests/Feature/AccountControllerTest.php
+++ tests/Feature/AccountControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use App\Services\AccountService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -20,19 +21,24 @@ class AccountControllerTest extends TestCase
 
     //arrange
     public function test_return_wishlist() {
-        $wishlist =
-        [
+        $wishlist = collect([
             ['title'=>'Clue'],
             ['title'=>'Battleship'],
             ['title'=>'Mr. Bacons Big Adventure Board Game'],
-        ];
+        ]);
+
+        /**
+         * @var User
+         * see https://laravel.com/docs/9.x/http-tests#session-and-authentication
+         */
+        $user = User::factory()->create();
 
         $this->accountServiceSpy->shouldReceive('getWishlistByUserId')
         ->once()
         ->andReturn($wishlist);
 
         //act
-        $response = $this->get('/wishlist');
+        $response = $this->actingAs($user)->get('/wishlist');
 
         //assert
         $response->assertStatus(200);
