5ed8f0a96af22b9b5e22468ee16f17fb7c5884ac
oldskoolfan <harris.1305@gmail.com>
2022-11-30 21:29:45 -0500

    Update routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/game/{id}', [AccountController::class, 'toggleWishlist']);
+Route::get('/toggle-wishlist/{id}', [AccountController::class, 'toggleWishlist']);
