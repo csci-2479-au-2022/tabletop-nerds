31ae6ebc4a787e5b031c2bde7256233bf4517c1c
Lil Dead01 <lildead01@icloud.com>
2022-12-08 17:40:34 -0500

    work on wishlist layout, remove blank lines from file

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -20,8 +20,6 @@ class AccountController extends Controller
 
     public function toggleWishlist(Request $request, int $gameId) {
         $userId = (int)$request->query('user');
-
-
         return response()->json($this->accountService->toggleWishlist($gameId, $userId));
     }
 }
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -1,15 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Your Wishlist') }}
+            {{ __('Game Wishlist') }}
         </h2>
     </x-slot>
-    <body>
-        <h1>This is your wishlist - hopefully you get what you wish for!</h1>
-        @foreach($wishlist as $game)
-        <p>{{$game['title']}}</p>
-        <x-game-info :game="$game" :shouldRemove="$shouldRemove" />
-        @endforeach
-    </body>
-    </html>
+    <x-main-body-content>
+        <table>
+            <thead>
+                <th>Your Wishlist</th>
+            </thead>
+            <tbody>
+            @foreach($wishlist as $game)
+                <p>{{$game['title']}}</p>
+                <tr>
+                    <td>
+                        <x-wishlist :game="$game" :shouldRemove="$shouldRemove" />
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </x-main-body-content>
 </x-app-layout>
